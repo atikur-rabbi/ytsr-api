0aba5bdb5092483b32b74d1ccbf065494a8239c9
atikur-rabbi <msawon94@gmail.com>
2020-09-02 13:18:07 +0600

    Updated at 2020-09-02_0118 PM

--- ytsr.js
+++ ytsr.js
@@ -1,19 +1,40 @@
 const ytsr = require('ytsr');
 let filter;
+ytsr.do_warn_deprecate = false;
 
-ytsr.getFilters('github', function(err, filters) {
-  if(err) throw err;
-  filter = filters.get('Type').find(o => o.name === 'Video');
-  ytsr.getFilters(filter.ref, function(err, filters) {
-    if(err) throw err;
-    filter = filters.get('Duration').find(o => o.name.startsWith('Short'));
-    var options = {
-      limit: 5,
-      nextpageRef: filter.ref,
-    }
-    ytsr(null, options, function(err, searchResults) {
-      if(err) throw err;
-      console.log(searchResults);
-    });
-  });
-});
+let sResult = "some val";
+let searchResults = [];
+var lavel = "github";
+
+ ytsr(lavel, function (err, searchResults) {
+    if (err) throw err;
+    sResult = JSON.stringify(searchResults)
+    return searchResults;
+  })
+
+async function testAsync() {
+ return "hello";
+}
+ 
+console.log(sResult)
+console.log(ytsr(lavel));
+console.log(testAsync());
+
+// ytsr.getFilters('github', function(err, filters) {
+
+//   if(err) throw err;
+//   filter = filters.get('Type').find(o => o.name === 'Video');
+//  // ytsr.do_warn_deprecate = false;
+//   ytsr.getFilters(filter.ref, function(err, filters) {
+//     if(err) throw err;
+//     filter = filters.get('Duration').find(o => o.name.startsWith('Short'));
+//     var options = {
+//       limit: 50,
+//       nextpageRef: filter.ref,
+//     }
+//     ytsr(null, options, function(err, searchResults) {
+//       if(err) throw err;
+//       console.log(searchResults);
+//     });
+//   });
+// });
